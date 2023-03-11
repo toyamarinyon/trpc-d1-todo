ccab39e93a5885dd9b8a12000e326c11337abde6
toyamarinyon <toyamarinyon@gmail.com>
2023-03-11 11:39:36 +0900

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+  root: true,
+};
+module.exports = config;
